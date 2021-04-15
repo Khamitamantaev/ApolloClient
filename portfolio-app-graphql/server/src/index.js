bd8ff8c623c0747d375cfb9d0027d2ea4b9817dd
Khamit Amantaev <khamitamantaev@gmail.com>
2021-04-16 00:14:59 +0500

    успешная вторая миграция с созданием пользователя

--- portfolio-app-graphql/server/src/index.js
+++ portfolio-app-graphql/server/src/index.js
@@ -1,42 +1,38 @@
 const { ApolloServer } = require("apollo-server");
-const fs = require('fs');
-const path = require('path');
+const { PrismaClient } = require("@prisma/client");
+const fs = require("fs");
+const path = require("path");
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
+const prisma = new PrismaClient();
 
-let idCount = links.length
+
 const resolvers = {
   Query: {
-    info: () => `This is the App`,
-    feed: () => links,
+    info: () => `This is the API for portfolio`,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
   },
   Mutation: {
-    // 2
-    post: (parent, args) => {
-       const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link)
-      return link
-    }
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink
+    },
   },
 }
 
-
-
 // 3
 const server = new ApolloServer({
-  typeDefs: fs.readFileSync(
-    path.join(__dirname, 'schema.graphql'),
-    'utf8'
-  ),
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
   resolvers,
+  context: {
+    prisma,
+  },
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
